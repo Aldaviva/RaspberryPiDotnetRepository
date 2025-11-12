8570f61b5228e750aa1c67048a07949bf2454bc3
Ben Hutchison <ben@aldaviva.com>
2025-11-11 21:49:30 -0800

    #36: Test HTTPS connection in Hello World program, because TLS compatibility is most at risk when mixing older versions of operating systems, runtimes, OpenSSL, and glibc (such as .NET 8 on 32-bit Trixie)

--- HelloWorldRuntime/Program.cs
+++ HelloWorldRuntime/Program.cs
@@ -1,3 +1,9 @@
-﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices;
 
-Console.WriteLine($"Hello from {RuntimeInformation.FrameworkDescription} {RuntimeInformation.RuntimeIdentifier} {RuntimeInformation.OSArchitecture} {(Environment.Is64BitOperatingSystem ? 64 : 32)}-bit");
+Console.WriteLine(
+    $"Hello from {RuntimeInformation.FrameworkDescription} {RuntimeInformation.RuntimeIdentifier} {RuntimeInformation.OSArchitecture} {(Environment.Is64BitOperatingSystem ? 64 : 32)}-bit");
+
+using HttpClient          httpClient = new();
+using HttpResponseMessage response   = await httpClient.GetAsync("https://aldaviva.com/");
+response.EnsureSuccessStatusCode();
+Console.WriteLine($"HTTPS request OK (HTTP {response.Version})");
